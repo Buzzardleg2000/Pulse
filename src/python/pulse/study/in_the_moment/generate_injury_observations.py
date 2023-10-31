e370e1006ac7b587603c07130728f92d481fd9ce
Aaron Bray <aaron.bray@kitware.com>
2023-10-31 11:16:15 -0400

    Read test file in itm pipeline

--- src/python/pulse/study/in_the_moment/generate_injury_observations.py
+++ src/python/pulse/study/in_the_moment/generate_injury_observations.py
@@ -2,11 +2,9 @@
 # See accompanying NOTICE file for details.
 
 import logging
-from typing import List
 
 from pulse.cdm.scenario import SEScenarioExecStatus
-from pulse.cdm.io.scenario import serialize_scenario_exec_status_list_from_file, \
-                                serialize_scenario_exec_status_list_to_file
+from pulse.cdm.io.scenario import serialize_scenario_exec_status_list_from_file
 
 _pulse_logger = logging.getLogger('pulse')
 
@@ -22,7 +20,7 @@ if __name__ == "__main__":
     #   This will make a patient scenario exec status list file
     # Execute all those scenarios
 
-    injury_scenarios_list_filename = "./test_results/scenarios/ScenarioDirectoryStatuses.json"
+    injury_scenarios_list_filename = "./test_results/patient_variability/test/scenarios/tccc.json"
     # Look for an injury scenario exec status list file
     # if it's not there
     # Call C++ to generate our tccc injury scenarios
@@ -36,5 +34,7 @@ if __name__ == "__main__":
     # Iterate over these injury scenario to
     for injury_scenario in injury_scenarios:
         print("Generating observations for "+str(injury_scenario.get_scenario_filename()))
-    # Call our post processor to generate observation files for each scenario
+        # Call our post processor to generate observation files for each scenario
+        # Observations should go here: (just replace the 'scenarios' in the csv results location)
+        # ./test_results/patient_variability/test/observations/tccc/Male_44yr_180.34cm_23.71bmi_0.21bff_72bpm_87mmHg_40.5mmHg_12bpm/AO0_H0.5_TP0_D5s.json
 
